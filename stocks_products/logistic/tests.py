b127d4d8cd94020052f109e3123f57cc011280db
Dmitry Godz <woke@mail.ru>
2023-08-09 22:52:34 +0300

    test5

--- stocks_products/logistic/tests.py
+++ stocks_products/logistic/tests.py
@@ -6,6 +6,6 @@ class TestView(TestCase):
     def test_response(self):
         url = '/api/v1/test'
         client = APIClient()
-        response = client.get(url)
+        response = client.get(url, follow=True)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, 'Hello, this is a test page')
