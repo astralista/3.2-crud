b84d3b235b9b4f7f8e92964cf41aef09efcf4082
Dmitry Godz <woke@mail.ru>
2023-08-31 14:26:33 +0300

    работает_ но со статикой все равно какие то ошибки

--- stocks_products/stocks_products/settings.py
+++ stocks_products/stocks_products/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-nw^y+m^wmxza1asgk+)!ua2qx9)g+#v=6%76-9i8i(6eqiw94j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -79,8 +79,8 @@ WSGI_APPLICATION = "stocks_products.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "netology_stocks_products",
-        "HOST": "127.0.0.1",
+        "NAME": "postgres_DB",
+        "HOST": "postgres",
         "PORT": "5432",
         "USER": "postgres",
         "PASSWORD": "postgres",
@@ -139,3 +139,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 3,
 }
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
