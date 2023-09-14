4bf0809d23a74d0915ce0638c78f80992fdf19db
Dmitry Godz <woke@mail.ru>
2023-09-14 17:49:45 +0300

    staticfiles

--- stocks_products/stocks_products/settings.py
+++ stocks_products/stocks_products/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = "/static/"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -140,4 +140,6 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 3,
 }
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
